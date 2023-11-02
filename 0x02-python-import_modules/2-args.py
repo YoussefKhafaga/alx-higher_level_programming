b022c9bb3edfccc93c9208968b6e438a29f6e398
YoussefKhafaga <ykhfaga@gmail.com>
2023-11-02 16:59:27 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    count = len(argv) - 1
+    print("{} argument{}".format(count, "s:" if count > 1 else ":" if count == 1 else "."))
+    for i, arg in enumerate(argv[1:]):
+        print("{}: {}".format(i + 1, arg))
